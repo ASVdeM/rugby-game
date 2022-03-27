e8d984e847cad70d9463f5ae95c0d8777ab001eb
ASVdeM <32139391+ASVdeM@users.noreply.github.com>
2022-03-27 06:58:56 +0000

    Criei uma estratégia para o defensor

--- src/defender.c
+++ src/defender.c
@@ -1,10 +1,12 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
 
 // Internal headers
 #include "direction.h"
 #include "position.h"
 #include "spy.h"
+#include "auxiliar.h"
 
 // Main header
 #include "defender.h"
@@ -17,13 +19,27 @@
 /*----------------------------------------------------------------------------*/
 
 direction_t execute_defender_strategy(
-    position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
-
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+    position_t defender_position, Spy attacker_spy){
+  UNUSED (attacker_spy);
+  
+  static position_t previous_position = INVALID_POSITION;
+  static direction_t next_direction;
+  
+  if (equal_positions (defender_position, previous_position)){
+    next_direction = random_direction ();
+  }
+  
+  else {
+    if (rand () % 2 == 0){
+      next_direction = (direction_t) {1, 0};
+    }
+    else{
+      next_direction = (direction_t) {-1, 0};
+    }
+  }
+      
+  previous_position = defender_position;
+  return next_direction;
 }
 
 /*----------------------------------------------------------------------------*/
