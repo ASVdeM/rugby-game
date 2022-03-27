2d5ee637d9c6f0c75cdb473fa2adf669e858a603
ASVdeM <32139391+ASVdeM@users.noreply.github.com>
2022-03-27 06:56:25 +0000

    Add files via upload

--- src/attacker.c
+++ src/attacker.c
@@ -1,16 +1,20 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Internal headers
 #include "direction.h"
 #include "position.h"
 #include "spy.h"
+#include "auxiliar.h"
 
 // Main header
 #include "attacker.h"
 
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
+#define random (rand () % 3) - 1 //isso dá um número inteiro entre -1 e 1 
 
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
@@ -18,12 +22,21 @@
 
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
-
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+    UNUSED (defender_spy);
+    srand (time (NULL));
+    
+    static position_t previous_position = INVALID_POSITION;
+    static direction_t next_direction;
+    
+    if (equal_positions (previous_position, attacker_position)){
+        next_direction = random_direction ();
+    }
+    else {
+        next_direction = (direction_t) {random, 1};
+    }
+    
+    previous_position = attacker_position;
+    return next_direction;
 }
 
 /*----------------------------------------------------------------------------*/
