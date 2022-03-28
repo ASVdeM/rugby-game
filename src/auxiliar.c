6af770d4895c9cf08fca081d9e51f94f35fe6350
ASVdeM <32139391+ASVdeM@users.noreply.github.com>
2022-03-28 13:12:22 -0300

    Melhorei o arquivo auxiliar

--- src/auxiliar.c
+++ src/auxiliar.c
@@ -1,11 +1,26 @@
-#include <stdlib.h>
 #include "auxiliar.h"
-direction_t random_direction (){
-	int x = (rand () % 3) - 1,
-	    y = (rand () % 3) - 1;
-	while (x == 0 && y == 0){
-		x = (rand () % 3) - 1;
-		y = (rand () % 3) - 1;
+
+int sign (int x){
+	return (x > 0) - (x < 0);
+}
+//visto em https://stackoverflow.com/a/1903975
+
+int power (int x, int y){
+	if (y == 0){
+		return 1;
 	}
-	return (direction_t) {x, y};
+	else {
+		return x * power (x, y - 1);
+	}
+}
+
+direction_t random_direction (direction_t forbidden_direction){
+	//eu tentei andar e não consegui, daí eu sorteio uma direção aletória que não seja forbidden_direction
+	unsigned int horizontal = rand () % 8, vertical = rand () % 8;
+	vertical = (vertical - 2) % 8;//isso é necessário por causa do jeito que as direções funcionam
+	direction_t next_direction = (direction_t) {sign (power (-1, horizontal/4) * (horizontal % 4)), sign (power (-1, vertical/4) * (vertical % 4))};
+	while (next_direction.i != forbidden_direction.i && next_direction.j != forbidden_direction.j){
+	next_direction = (direction_t) {sign (power (-1, horizontal/4) * (horizontal % 4)), sign (power (-1, vertical/4) * (vertical % 4))};
+	}
+	return next_direction;
 }
