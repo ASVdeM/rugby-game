0db2ea90f4607418b348629171ba09a03490865d
ASVdeM <32139391+ASVdeM@users.noreply.github.com>
2022-03-28 13:10:04 -0300

    Melhorei a estratégia do atacante

--- src/attacker.c
+++ src/attacker.c
@@ -14,7 +14,6 @@
 
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
-#define random (rand () % 3) - 1 //isso dá um número inteiro entre -1 e 1 
 
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
@@ -22,21 +21,33 @@
 
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-    UNUSED (defender_spy);
-    srand (time (NULL));
-    
-    static position_t previous_position = INVALID_POSITION;
-    static direction_t next_direction;
-    
-    if (equal_positions (previous_position, attacker_position)){
-        next_direction = random_direction ();
+  //Estratégia: Após 2 turnos, Eu uso o espião para Ver aonde está O Defensor E Daí eu vou na Direção Oposta (Direita e Acima Se Ele estiver abaixo, Direita E Abaixo se Estiver Acima, E Aleatório se estiver na Mesma altura)
+  static position_t previous_position = INVALID_POSITION;
+  static direction_t direction;
+  static int rounds = 0;
+  if (equal_positions (previous_position, attacker_position)){
+    direction = random_direction (direction);
+  }
+  else {
+    if (rounds > 2){
+      if (get_spy_number_uses (defender_spy) > 0) {
+        position_t defender_position = get_spy_position (defender_spy);
+        int height_difference = attacker_position.i - defender_position.i;
+	    if (height_difference != 0){
+	      direction = (direction_t) {-height_difference, 1};
+	    }
+        else{
+	      direction = (direction_t) {power (-1, rand () % 2), 1};
+        }
+      }
     }
     else {
-        next_direction = (direction_t) {random, 1};
+      direction = (direction_t) DIR_RIGHT;
     }
-    
-    previous_position = attacker_position;
-    return next_direction;
+  }
+  rounds++;
+  previous_position = attacker_position;
+  return direction;
 }
 
 /*----------------------------------------------------------------------------*/
