105028827adc8da347390eba790d3864f050c2ec
ASVdeM <32139391+ASVdeM@users.noreply.github.com>
2022-03-28 13:06:59 -0300

    Melhorei o auxiliar.h

--- include/auxiliar.h
+++ include/auxiliar.h
@@ -1,2 +1,5 @@
 #include "direction.h"
-direction_t random_direction(); 
+#include <stdlib.h>
+int sign (int x);
+int power (int x, int y);
+direction_t random_direction (direction_t forbidden_direction);
